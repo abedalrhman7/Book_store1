7a804e87760ed8d40e38f6a5453f48cfc4db3405
Abedalrhman <king16374@gmail.com>
2025-05-11 13:29:58 +0300

    Book_store1

--- app/src/main/java/com/example/book_store1/MainActivity.java
+++ app/src/main/java/com/example/book_store1/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.book_store1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.latest_books);
+        setContentView(R.layout.main_card_book);
+        ImageButton points3Button = findViewById(R.id.Points3);
+        ImageButton points2Button = findViewById(R.id.Points2);
 
+        points3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CardBookLatestActivity.class);
 
-    }
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "ImageButton clicked!", Toast.LENGTH_SHORT).show();
+            }
+            });
 
+        points2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CardBookTrendingActivity.class);
+
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "ImageButton clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        }
 
 }
