1c8dd2ac97843f14506d8e085e3e7ff9f4088b06
Abedalrhman <king16374@gmail.com>
2025-05-26 21:02:15 +0300

    Book_store1

--- app/src/main/java/com/example/book_store1/MainActivity.java
+++ app/src/main/java/com/example/book_store1/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.main_card_book);
+        setContentView(R.layout.activity_main);
         ImageButton points3Button = findViewById(R.id.Points4);
         ImageButton points2Button = findViewById(R.id.Points2);
         ImageButton pointsBestButton = findViewById(R.id.pointsBest);
